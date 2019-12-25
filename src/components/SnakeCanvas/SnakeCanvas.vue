6ba0711773ba54f46d08653cab1b5f2a5da8a313
TPGT00020 <le_thi_cuc@tatsuno.co.jp>
2019-12-25 13:54:20 +0900

    Snake game on vuejs

---
+++ src/components/SnakeCanvas/SnakeCanvas.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <canvas ref="board" id="snake-canvas" :width="boardSizePx" :height="boardSizePx"></canvas>
+    </div>
+</template>
+
+<script src="./SnakeCanvas.js"></script>
+
+<style scoped src="./SnakeCanvas.css"></style>
